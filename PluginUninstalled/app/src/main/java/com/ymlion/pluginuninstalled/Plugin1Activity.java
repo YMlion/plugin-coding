5f39551cb1caefeda69d2640899d375396d66081
YMlion <lm_yao@163.com>
2018-03-12 21:09:32 +0800

    opt

--- PluginUninstalled/app/src/main/java/com/ymlion/pluginuninstalled/Plugin1Activity.java
+++ PluginUninstalled/app/src/main/java/com/ymlion/pluginuninstalled/Plugin1Activity.java
@@ -1,12 +1,19 @@
 package com.ymlion.pluginuninstalled;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Plugin1Activity extends Activity {
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plugin1);
+        findViewById(R.id.open_p2).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                startActivity(new Intent(Plugin1Activity.this, Plugin2Activity.class));
+            }
+        });
     }
 }
